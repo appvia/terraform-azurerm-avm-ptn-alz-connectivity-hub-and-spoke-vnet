35d216de04438c12cad0a44ed264301d19f2135f
Jared Holgate <jaredholgate@microsoft.com>
2025-06-10 17:30:33 +0100

    fix: replace azurerm provider (#19)

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "azure/azapi"
       version = "~> 2.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
