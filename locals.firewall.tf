01f8ca760db1c9c2bf3faba28dab79c6657c96e6
Sam Hakkak <sam.hakkak@appvia.io>
2025-08-07 16:51:41 +0100

    adding basic sku option

--- locals.firewall.tf
+++ locals.firewall.tf
@@ -1,7 +1,7 @@
 locals {
   firewall_enabled = { for key, value in var.hub_virtual_networks : key => try(value.hub_virtual_network.firewall.enabled, try(value.hub_virtual_network.firewall, null) != null) }
   firewall_policies = { for key, value in var.hub_virtual_networks : key => local.firewall_enabled[key] ? merge(value.hub_virtual_network.firewall.firewall_policy, {
-    dns = try(value.hub_virtual_network.firewall.sku_tier, "") == "Basic" ? null : try(value.hub_virtual_network.firewall.firewall_policy.dns, local.firewall_policy_dns_defaults[key])
+    dns = try(value.hub_virtual_network.firewall.firewall_policy.sku, "") == "Basic" ? null : try(value.hub_virtual_network.firewall.firewall_policy.dns, local.firewall_policy_dns_defaults[key])
   }) : null }
   firewall_policy_dns_defaults = { for key, value in var.hub_virtual_networks : key => local.private_dns_resolver_enabled[key] && local.private_dns_zones_enabled[key] && local.firewall_enabled[key] ? {
     proxy_enabled = true
